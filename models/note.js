499f710450507f26c31e5088fd58640665c19433
seldesoky <sameh.s.eldesoky@gmail.com>
2024-07-28 13:38:57 -0400

    final commit

--- models/note.js
+++ models/note.js
@@ -2,11 +2,21 @@ const mongoose = require('mongoose')
 
 const noteSchema = new mongoose.Schema ({
     text: {type: String, required: true},
-    isRead: {type: Bookean, required: true},
+    isRead: {type: Boolean, required: true},
 },{
     timestamps: true    
-
 })
 
 const Note  = mongoose.model('Note', noteSchema)
+
 module.exports = Note
+
+//Example code. Articles
+
+// const articleSchema = new mongoose.Schema({
+//     title: {type: String, required: true},
+//     body: {type: String, required: true},
+//     slug: {type: String, required: true, unique: true}
+
+
+// })
--- server.js
+++ server.js
@@ -1,22 +1,88 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const mongoose = require(mongoose)
+const mongoose = require('mongoose')
 const MONGO_URI = process.env.MONGO_URI
+const PORT = 3000
+const Note = require('./models/note')
+const logger = require('morgan')
+
+app.use(express.json()) // We are able to parse the body an accept json data form requestors
+app.use(logger('tiny'))
 
 mongoose.connect(MONGO_URI)
 
 mongoose.connection.once('open', () => {
-    console.log('MongoDB is connected')
+    console.log('MongoDB is showing love')
 })
 
 mongoose.connection.on('error', () => {
-    console.error('You have a stupid error')
+    console.error('You know how MongoDB be trippin')
 })
 
-// controller & r
+// controller & router logic
 
-// Create 
+// Create
 app.post('/notes', async (req, res) => {
-    
+    try {
+        const createdNote = await Note.create(req.body)
+        res.json(createdNote)
+    } catch (error) {
+        res.status(400).json({ msg: error.message })
+    }
 })
+
+// Read
+
+// Index and Show
+app.get('/notes', async (req, res) => {
+    try {
+        //console.log(req.query.example)
+        const foundNotes = await Note.find({})
+        res.json(foundNotes)
+    } catch (error) {
+        res.status(400).json({ msg: error.message })
+    }
+})
+
+app.get('/notes/:id', async (req, res) => {
+    try {
+        const foundNote = await Note.findOne({ _id: req.params.id })
+        res.json(foundNote)
+    } catch (error) {
+        res.status(400).json({ msg: error.message})
+
+    }
+
+})
+
+// Update
+app.put('/notes/:id', async (req, res) => {
+    try {
+        const updatedNote = await Note.findByIdAndUpdate(
+            { _id: req.params.id } , req.body, { new: true })
+            res.json(updatedNote)
+
+    } catch (error) {
+        res.status(400).json({ msg: error.message })
+    }
+})
+
+// Delete
+app.delete('/notes/:id', async (req, res) => {
+    try {
+        await Note.findOneAndDelete({ _id: req.params.id })
+        .then((note) => {
+            res.sendStatus(204)
+        })
+
+    } catch (error) {
+        res.status(400).json({ msg: error.message})
+    }
+})
+
+// port listen
+app.listen(PORT, () => {
+    console.log('We in the buidling, ' + `application excepting requests on port 3000 `)
+})
+
